f4f2b361a3dfe6f96c220ea7c11f6b6e9fe419be
GabrielMarquezan <marquezan.luz@acad.ufsm.br>
2024-04-10 11:47:02 -0300

    Exercício referente à aula 9

---
+++ aula9.c
@@ -0,0 +1,18 @@
+#include "ts.h"
+
+int main()
+{
+    t1(4);
+    printf("\n");
+    t2(4);
+    printf("\n");
+    t3(4);
+    printf("\n");
+    t4(4);
+    printf("\n");
+    t5(4);
+    printf("\n");
+    t6(4);
+    printf("\n");
+    t7(4);
+}
